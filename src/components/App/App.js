a3b902ae17fe955182e1cff9be77ff9f999365d7
Heather Hartley <hlhartley@gmail.com>
2019-01-23 16:50:27 -0700

    Adds methods to App and Navigation components to render data basesd on button clicked

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,12 +11,17 @@ class App extends Component {
     this.state = {
       navFixed: false,
       initialNavPosition: 0,
-      people: []
+      people: [],
+      planets: [],
+      vehicles: [],
+      selected: ''
     };
   }
 
   componentDidMount() {
     this.fetchPeople()
+    this.fetchPlanets()
+    this.fetchVehicles()
     window.addEventListener('scroll', this.fixNav.bind(this))
     this.setState({initialNavPosition: 250})
   }
@@ -36,6 +41,32 @@ class App extends Component {
       .catch(error => console.log(error))
   }
 
+  fetchPlanets() {
+    fetch('https://swapi.co/api/planets/')
+      .then(response => response.json())
+      .then(result => this.setState({planets: result.results}))
+      .catch(error => console.log(error))
+  }
+
+  fetchVehicles() {
+    fetch('https://swapi.co/api/vehicles/')
+      .then(response => response.json())
+      .then(result => this.setState({vehicles: result.results}))
+      .catch(error => console.log(error))
+  }
+
+  receiveSelected = (selectedButton) => {
+    this.setState({ selected: selectedButton })
+  }
+
+  returnCards = () => {
+    if(this.state.selected === 'people') {
+      return this.state.people       
+    } else {
+      return []
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -44,10 +75,14 @@ class App extends Component {
           navFixed={this.state.navFixed}
           iitialNavPosition={this.state.initialNavPosition}
           people={this.state.people}
+          receiveSelected={this.receiveSelected}
+          // fetchPeople={this.fetchPeople}
+          // fetchPlanets={this.fetchPlanets}
+          // fetchVehicles={this.fetchVehicles}
         />
         {/* <ScrollText /> */}
         <h2 className='card-container-title'>PEOPLE:</h2>
-        <CardContainer people={this.state.people}/>
+        <CardContainer people={this.returnCards()}/>
       </div>
     );
   }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../main.scss';
 
-const Navigation = (props) => {
+class Navigation extends Component {
+    constructor() {
+        super();
+    }
+
+    passSelected = (e) => {
+        e.preventDefault()
+        this.props.receiveSelected(e.target.name)
+    }
+
+    render() {
     return (
-        <div className={ props.navFixed ? "navigation-container fixed-nav" : "navigation-container" }>
+        <div className={ this.props.navFixed ? "navigation-container fixed-nav" : "navigation-container" }>
             <div className='logo'>SWAPI-BOX</div>
-            <a className='people-link' href='www.google.com'><i className="fas fa-male"></i> PEOPLE [{props.people.length}]</a>
-            <a className='planets-link' href='www.google.com'><i className="fas fa-globe"></i> PLANETS</a>
-            <a className='vehicles-link' href='www.google.com'><i className="fas fa-fighter-jet"></i> VEHICLES</a>
+            <a className='people-link' href='www.google.com' name='people' onClick={this.passSelected.bind(this)}><i className="fas fa-male"></i> PEOPLE</a>
+            <a className='planets-link' href='www.google.com' name='planets' onClick={this.passSelected.bind(this)}><i className="fas fa-globe"></i> PLANETS</a>
+            <a className='vehicles-link' href='www.google.com' name='vehicles' onClick={this.passSelected.bind(this)}><i className="fas fa-fighter-jet"></i> VEHICLES</a>
             <a className='favorites-link' href='www.google.com'><i className="far fa-star"></i> FAVORITES [0]</a>
         </div>
     )
+}
 }
 
 export default Navigation;

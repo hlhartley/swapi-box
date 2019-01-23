1ecea50d8e217f0e592e316661f9972a0f9cda44
Heather Hartley <hlhartley@gmail.com>
2019-01-22 20:53:28 -0700

    Adds font-awesome icons to navigation bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,10 +5,10 @@ const Navigation = (props) => {
     return (
         <div className={ props.navFixed ? "navigation-container fixed-nav" : "navigation-container" }>
             <div className='logo'>SWAPI.</div>
-            <a href='www.google.com'>People</a>
-            <a href='www.google.com'>Planets</a>
-            <a href='www.google.com'>Vehicles</a>
-            <a href='www.google.com'>View Favorites</a>
+            <a href='www.google.com'><i class="fas fa-male"></i> PEOPLE</a>
+            <a href='www.google.com'><i class="fas fa-globe"></i> PLANETS</a>
+            <a href='www.google.com'><i class="fas fa-fighter-jet"></i> VEHICLES</a>
+            <a href='www.google.com'><i class="far fa-star"></i> FAVORITES</a>
         </div>
     )
 }

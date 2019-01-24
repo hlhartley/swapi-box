0a504d9e0b84e062839748c6c9ea339dbf4de0f0
Heather Hartley <hlhartley@gmail.com>
2019-01-24 15:37:58 -0700

    Fixes scroll text bug

--- src/components/ScrollText/ScrollText.js
+++ src/components/ScrollText/ScrollText.js
@@ -11,10 +11,7 @@ const ScrollText = (props) => {
             <p>Episode IV</p>
             <h1>{props.film.title}</h1>
             </div>
-            <p>{props.film.ScrollText}</p>
-            <p>It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>     
-            <p>During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.</p>
-            <p>Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…</p>
+            <p className='scroll-text'>{props.film.scrollText}</p>
         </div>
         </section>
         </div>
